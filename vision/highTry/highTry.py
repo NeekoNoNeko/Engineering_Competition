3014b74533d6bba75eceada427297b893f3e5934
Neeko <116475831+NeekoNoNeko@users.noreply.github.com>
2024-09-20 00:12:27 +0800

    计算两点（未完成

--- vision/highTry/highTry.py
+++ vision/highTry/highTry.py
@@ -1,8 +1,11 @@
 # Untitled - By: sonkin - Fri Aug 30 2024
 #找色块后找亮光
 
-import sensor, image, time, pyb
+import sensor, image, time, pyb, math
 from machine import UART
+
+from vision.highTry.test_file.ClosestPair import closest_pair
+
 red_threshold = [(0, 50, 4, 60, 0, 60)]
 #(9, 35, -9, 36, -31, 28)
 blue_threshold = [(0, 60, -10, 127, -128, -10)]
@@ -101,6 +104,85 @@ class SendData:
 
     #    print("has been sent")
 
+
+class ClosestPair:
+    point_list = []
+
+    def get_point_list(self, x):
+        self.point_list.append(x)
+
+    def calculation(self):
+        distances, pair = self.closest_pair(self.point_list, 0, len(self.point_list) - 1)
+        print("min distance:", distances)
+        print("The closest pair is:", pair)
+        return pair
+
+    # 定义一个函数来计算两点之间的欧几里得距离
+    def dist(self,a, b):
+        return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+    # 定义一个函数来找到一组点中的最近点对
+    def closest_pair(self, points, left, right):
+        # 当点的数量少于等于3时，直接使用暴力方法计算最小距离
+        if right - left <= 3:
+            return self.brute_force(points[left:right + 1])
+
+        # 找到中点，将点集分成两部分
+        mid = (left + right) // 2
+        p1 = points[left:mid + 1]
+        p2 = points[mid + 1:right + 1]
+
+        # 递归地在两个子集中找到最近点对
+        d1, pair1 = self.closest_pair(points, left, mid)
+        d2, pair2 = self.closest_pair(points, mid + 1, right)
+        d_min = min(d1, d2)
+        best_pair = pair1 if d1 <= d2 else pair2
+
+        # 合并两个子集，并找到横跨中点的最近点对
+        merge_points = self.merge(p1, p2)
+        d_strip, strip_pair = self.closest_in_strip(merge_points, d_min)
+        if d_strip < d_min:
+            d_min = d_strip
+            best_pair = strip_pair
+
+        return d_min, best_pair
+
+    # 合并两个子集的函数
+    def merge(self, p1, p2):
+        # 合并两个列表，并按照y坐标排序
+        return sorted(p1 + p2, key=lambda x: x[1])
+
+    # 找到横跨中点的最近点对的函数
+    def closest_in_strip(self, points, d_min):
+        # 按照x坐标排序
+        points = sorted(points, key=lambda x: x[0])
+        d = d_min
+        best_pair = None
+
+        # 遍历每个点，找到横跨中点的最近点对
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                if points[j][0] - points[i][0] >= d:
+                    break
+                if self.dist(points[i], points[j]) < d:
+                    d = self.dist(points[i], points[j])
+                    best_pair = (points[i], points[j])
+
+        return d, best_pair
+
+    def brute_force(self, points):
+        # 初始化最小距离为无穷大
+        min_dist = math.inf
+        best_pair = None
+        # 计算所有点对的距离，找出最小值
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                if self.dist(points[i], points[j]) < min_dist:
+                    min_dist = self.dist(points[i], points[j])
+                    best_pair = (points[i], points[j])
+        # 返回最小距离和最近点对
+        return min_dist, best_pair
+
 #data = uart.read().decode()
 class Movement:
 #    _mode = {0 : "mode1", 1 : "mode2", 2 : "mode3"}
@@ -112,6 +194,7 @@ class Movement:
 
     def catch(self):
         print("catch")
+        closest_pair_calculation = ClosestPair()
         sensor.skip_frames(time=3000)
         img = sensor.snapshot().lens_corr(strength = 1.5, zoom = 1.0)# 消除镜头鱼眼畸变
         blobs = img.find_blobs(self.colour, area_threshold=300, margin=10)# type(blob) is list
@@ -144,6 +227,9 @@ class Movement:
                 img.erode(2)
 
                 bright_spot = img.find_blobs([(0, 76, -128, 127, -128, 127)], area_threshold=10, margin=100)
+                for b in bright_spot:
+                    closest_pair_calculation.get_point_list( (b.x(), b.y()) )
+                closest_pair_calculation.calculation()
 
                 img.draw_cross(bright_spot[0].cx(), bright_spot[0].cy(), color=(0, 255, 0))
                 print("X:", bright_spot[0].cx(), " Y:", bright_spot[0].cy())
